370ae5acec039dfa42d8dcb5aef66ac5d0e529f9
Tiffany Flor <taflor@eckerd.edu>
2020-08-27 15:23:27 -0500

    set up initial framework of project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Covid hospital cases in the USA by county',
+    author='Tiffany Flor',
+    license='MIT',
+)
